900a194d7514b37d880eaa87572fc6db4e35a545
sandy-tartari <62676992+sandy-tartari@users.noreply.github.com>
2020-06-26 16:25:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ler5Nomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] nomes = new string [5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite um nome:");
+                nomes [i]  = Console.ReadLine();
+            }
+            Array.Reverse(nomes);
+            foreach (string i in nomes)
+            {
+                Console.WriteLine(""+ i);
+            }
+        }  
+    }
+}
